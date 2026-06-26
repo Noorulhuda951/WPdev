b83b12b3f765154433dafe081a316b986495ff82
Noorulhuda951 <muhammadnuur786@gmail.com>
2026-06-26 05:00:41 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -201,7 +201,3 @@
         }, 5000);
 
         // Form submission
-        document.querySelector('form').addEventListener('submit', (e) => {
-            e.preventDefault();
-            alert('Thank you for your message! I will get back to you soon.');
-        });
